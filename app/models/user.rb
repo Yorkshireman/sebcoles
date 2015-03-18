b5ca74337d799cf09769dc26b21d14a2824419c3
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-03-18 01:01:45 +0100

    followed groupify steps, ready to test in console

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  groupify :group_member
+  groupify :named_group_member
+end
+
+class Assignment < ActiveRecord::Base
+  groupify :group_member
 end
