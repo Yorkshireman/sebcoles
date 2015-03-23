dd4b6ef17188ce9461b62c69ee9369dc2ce4d2b5
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-03-23 03:48:17 +0100

    added type column to users table for sti

---
+++ db/migrate/20150323024523_add_type_column_to_users_table.rb
@@ -0,0 +1,9 @@
+class AddTypeColumnToUsersTable < ActiveRecord::Migration
+  def up
+  	add_column :users, :type, :string
+  end
+
+  def down
+  	remove_column :users, :type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323020848) do
+ActiveRecord::Schema.define(version: 20150323024523) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150323020848) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
