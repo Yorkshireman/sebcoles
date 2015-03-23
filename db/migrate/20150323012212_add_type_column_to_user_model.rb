33a7a6c3d18d255849a3f9fa65a9489e511bef91
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-03-23 02:56:44 +0100

    removed type column from user table and deleted the migration file

--- db/migrate/20150323012212_add_type_column_to_user_model.rb
+++
@@ -1,9 +0,0 @@
-class AddTypeColumnToUserModel < ActiveRecord::Migration
-  def up
-  	add_column :users, :type, :string, default: "Student"
-  end
-
-  def down
-  	remove_column :users, :type
-  end
-end
