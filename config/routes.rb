c6619c94b9463512086cc4d8731d639d0dc5152e
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-03-27 18:01:06 +0100

    created admin view page, but no functionality yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  get 'static_pages/admin'
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
