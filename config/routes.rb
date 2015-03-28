b7e2fd11e9471e16826ef236798a670e0f259721
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-03-28 03:42:55 +0100

    changed admin page url

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  get 'static_pages/admin'
   root 'static_pages#home'
+  devise_for :users
+  get 'admin' => 'static_pages#admin'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
