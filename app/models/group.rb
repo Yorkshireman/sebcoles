034c14d5b47d284b02a4a7a386a428e025daa30f
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-03-28 05:06:49 +0100

    Groups index page and creation sorted, but still need to code assignments

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
 	belongs_to :teacher
 	has_and_belongs_to_many :students, join_table: "groups_students"
+	validates :title, presence: :true, length: {maximum: 60}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
+
   devise_for :users
+  
   get 'admin' => 'static_pages#admin'
+
+  resources :groups
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
