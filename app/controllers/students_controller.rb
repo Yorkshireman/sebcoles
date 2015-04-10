a45a4de4191a6ed40dd2d69b08afca019c65538e
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-04-10 18:08:25 +0200

    build a submit form for students, but can't get submit action to work

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,10 @@ class StudentsController < UsersController
 		@groups = Group.all
 	end
 
+	def assign_to_group
+		@user=User.find(params[:user_id])
+		@group=Group.find(params[:title])
+		@user.groups << @group
+	end
+
 end
