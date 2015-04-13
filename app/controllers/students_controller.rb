4302a07fa56b3758b34f6697b6a7670854545098
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-04-13 12:49:44 +0200

    installed simple_form and started working out of the edit view instead of the show view. Still doesn't work though!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,10 +7,27 @@ class StudentsController < UsersController
 		@groups = Group.all 	# So Groups can be shown in a Student's show view
 	end
 
-	def assign_to_group
-		#@user=User.find(params[:user])
-		#@group=Group.find(params[:title])
-		@user.groups << @group
+	def update
+		respond_to do |format|
+      		if @user.update(user_params)
+        		format.html { redirect_to student_path, notice: 'Student was successfully updated.' }
+        		format.json { render :show, status: :ok, location: @user }
+      		else
+        		format.html { render :edit }
+        		format.json { render json: @user.errors, status: :unprocessable_entity }
+      		end
+    	end
 	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:student).permit(:group_ids)
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params[:user].permit(:type)
+      params.require(:user).permit(:type)
     end
 end
