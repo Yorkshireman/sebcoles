5df5f3698a0ce0a2dd69fe879e271bc144fc86ed
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-04-21 16:16:30 +0200

    more styling

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,14 @@
 module StaticPagesHelper
+	
+	def any_unassigned_users?
+  	array = Array.new
+  	users = User.all
+  	users.each do |user|
+  		if user.type == nil
+  			array << user
+  		end
+  	end
+  	array.count > 0
+	end
+
 end
