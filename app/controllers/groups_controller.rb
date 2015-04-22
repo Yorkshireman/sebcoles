5c1c8b4a6b0735d26da476b0025d4dcdb0f83e49
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-04-22 02:38:29 +0200

    notice isn't displayed on group update (disabled in controller); more styling applied to admin page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -42,7 +42,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_path, notice: 'Group was successfully updated.' }
+        format.html { redirect_to group_path}#, notice: 'Group was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit }
