063eec507f7e8590d343300bf5e6e530e686ec87
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-04-30 01:44:22 +0200

    wrote user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 describe User do
-	it "has a valid factory"
-	it "is invalid without a first_name"
-	it "is invalid without a last_name"
-	it "returns a user's full name as a string"
+
+	it "has a valid factory" do
+		expect(FactoryGirl.create(:user)).to be_valid
+	end
+
+	it "is invalid without an email" do
+		expect(FactoryGirl.build(:user, email: nil)).to_not be_valid
+	end
+	
+	it "is invalid without a first_name" do
+		expect(FactoryGirl.build(:user, first_name: nil)).to_not be_valid
+	end
+	
+	it "is invalid without a last_name" do
+		expect(FactoryGirl.build(:user, last_name: nil)).to_not be_valid
+	end
+	
+	it "returns a user's full name as a string" do
+		user = FactoryGirl.create(:user, first_name: "John", last_name: "Smith")
+		expect(user.full_name).to eq "John Smith"
+	end
+
 end
