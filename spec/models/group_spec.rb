c89eac9a0e281f4df29d661b7515adcdd36c3e2f
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-05-01 02:32:09 +0200

    group_spec model validation written

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,4 +4,9 @@ describe Group do
 	it "has a valid factory" do
 		expect(FactoryGirl.create(:group)).to be_valid
 	end
+
+	it "is invalid without a title" do
+		expect(FactoryGirl.build(:group, title: nil)).to_not be_valid
+	end
+
 end
