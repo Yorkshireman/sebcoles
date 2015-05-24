9c90f6fdd19c237ca91d42457f9bf01a5851feee
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-05-24 20:48:32 +0200

    more tests

--- spec/controllers/testimonials_controller_spec.rb
+++ spec/controllers/testimonials_controller_spec.rb
@@ -5,6 +5,10 @@ def successful_render_of(template)
 	expect(response).to render_template(template)
 end
 
+def log_in(user_type)
+	sign_in FactoryGirl.create(user_type)
+end
+
 describe TestimonialsController do 
 
 	describe "GET #index" do
@@ -35,16 +39,27 @@ describe TestimonialsController do
 
 	describe "POST #create" do 
 		context "with VALID attributes" do 
-			it "creates new testimonial"
-				# expect {
-				# 	post :create, testimonial: FactoryGirl.attributes_for(:testimonial)
-				# 	}.to change(Testimonial, :count).by(1)
-			
+			it "creates new testimonial" do
+				log_in(:student)
+				expect {
+						post :create, testimonial: FactoryGirl.attributes_for(:testimonial)
+				}.to 	change(Testimonial, :count).by(1)
+			end
 		end
 
 		context "with INvalid attributes" do
-			it "does not create a new testimonial"
-			it "re-renders the #new method"
+			it "does not create a new testimonial" do 
+				log_in(:student)
+				expect {
+					post :create, testimonial: FactoryGirl.attributes_for(:invalid_testimonial)
+				}.to 	change(Testimonial, :count).by(0)
+			end
+
+			it "re-renders the #new method" do 
+				log_in(:student)
+				post :create, testimonial: FactoryGirl.attributes_for(:invalid_testimonial)
+				expect(response).to render_template(:new)
+			end
 		end
 	end
 
@@ -54,26 +69,76 @@ describe TestimonialsController do
 		end
 
 		context "valid attributes" do
-			it "locates the requested @testimonial"
-			it "changes @testimonial's attributes"
-			it "redirects to the home page"
+
+			it "locates the requested @testimonial" do 
+				put :update, id: @testimonial, testimonial: FactoryGirl.attributes_for(:testimonial)
+				expect(assigns :testimonial).to eq @testimonial				
+			end
+
+			it "changes @testimonial's attributes" do 
+				put :update, id: @testimonial,
+					testimonial: FactoryGirl.attributes_for(:testimonial, content: "edited blah")
+				@testimonial.reload
+				expect(@testimonial.content).to eq("edited blah")
+			end
+
+			it "redirects to the home page" do 
+				put :update, id: @testimonial, testimonial: FactoryGirl.attributes_for(:testimonial)
+				expect(response).to redirect_to root_path
+			end
 		end	
 
 		context "invalid attributes" do
-			it "locates the requested @testimonial"
-			it "does not change @testimonial's attributes"
-			it "re-renders the edit method"
+			it "locates the requested @testimonial" do 
+				put :update, id: @testimonial, testimonial: FactoryGirl.attributes_for(:testimonial)
+				expect(assigns :testimonial).to eq @testimonial				
+			end
+
+			it "does not change @testimonial's attributes" do 
+				put :update, id: @testimonial,
+					testimonial: FactoryGirl.attributes_for(:testimonial, content: "")
+				@testimonial.reload
+				expect(@testimonial.content).to_not eq("")				
+			end
+
+			it "re-renders the edit method" do 
+				put :update, id: @testimonial, testimonial: FactoryGirl.attributes_for(:invalid_testimonial)
+				expect(response).to render_template :edit
+			end
 		end
 	end
 
-	describe "DELETE destory" do
+	describe "DELETE destroy" do
 		before :each do
 			@testimonial = FactoryGirl.create :testimonial
 		end
 
-		it "deletes the testimonial"
-		it "redirects to home page"
+		it "deletes the testimonial" do
+			allow(controller).to receive(:can_delete_testimonial?).and_return(true)
+			expect{
+				delete :destroy, id: @testimonial
+			}.to change(Testimonial, :count).by(-1)
+		end
 
+		it "redirects to home page" do 
+			delete :destroy, id: @testimonial
+			expect(response).to redirect_to root_path
+		end
+
+		it "allows deletion of any testimonial by an admin user" do
+			def destroy_testimonial(user_type)
+				log_in(user_type)
+				expect{
+							delete :destroy, id: @testimonial
+				}.to 	change(Testimonial, :count).by(-1)
+				sign_out(:user)
+			end
+
+			destroy_testimonial(:admin_user)
+
+			@testimonial = FactoryGirl.create :testimonial
+			
+			destroy_testimonial(:teacher_admin_user)
+		end
 	end
-
 end
--- spec/factories/testimonials.rb
+++ spec/factories/testimonials.rb
@@ -4,4 +4,8 @@ FactoryGirl.define do
 	factory :testimonial do |f|
 		f.content { Faker::Lorem.paragraph }
 	end
+
+	factory :invalid_testimonial, parent: :testimonial do |f|
+		f.content ""
+	end
 end
